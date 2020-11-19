fadd3d51c273172d52d1a1c78f53d347ff30ac77
rocfrank <rocfrank@iu.edu>
2020-11-18 21:23:57 -0500

    Creates a tree script that randomizes the sizes, rotations, and mesh of placed trees.

---
+++ Canyon Run/Assets/Scripts/PalmTree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalmTree : MonoBehaviour
+{
+    // References
+    public Mesh[] variants;
+    public int minSize;
+    public int maxSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int size = Random.Range(minSize, maxSize + 1);
+        int variant = Random.Range(0, variants.Length);
+        int rotation = Random.Range(0, 360);
+        transform.localScale = new Vector3(size, size, size);
+        GetComponent<MeshFilter>().mesh = variants[variant];
+        transform.localEulerAngles = new Vector3(0, rotation, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
