d1bec8367d2ad19fc344f7b1665486670ba97a3f
rocfrank <rocfrank@iu.edu>
2020-12-04 13:35:25 -0500

    Adds an AGM-65F ATG missile as well as a video feed from the LITENING targeting pod on the bottom of the aircraft

---
+++ Canyon Run/Assets/Scripts/AGM_65F.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AGM_65F : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
