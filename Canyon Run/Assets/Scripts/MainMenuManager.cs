6811fe218eead2cf7bc6be752376112fdb16953c
rocfrank <rocfrank@iu.edu>
2020-12-13 15:33:07 -0500

    Adds a main menu with explanation of controls and starts on a mission briefing menu.

---
+++ Canyon Run/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Loads the mission briefing screen
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Exits the application completely
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
